d68b0a36cf1003f35649febbd7ed6fd5eaa6bfb6
abrar shams chowdhury <abrar.chowdhury99@gmail.com>
2020-10-20 19:37:20 +0600

    portfolio grid added

--- src/components/ProjectCard/Card.jsx
+++ src/components/ProjectCard/Card.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import './card.styles.scss'
 function Card() {
     return (
-        <div>
-            <p>the card</p>
+        <div className="card">
+            <div className="card-wrapper">
+                <img src="/assets/portfolio.jpg" alt=""/>
+                <div className="caption">
+                    <p>Portfolio</p>
+                </div>
+            </div>    
         </div>
     )
 }
--- src/components/portfolioGrid/GridList.jsx
+++ src/components/portfolioGrid/GridList.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Card from '../ProjectCard/Card'
+import Grid from '@material-ui/core/Grid';
+const numbers = [1,2,3,4,5,6,7,8,9]
+
 function GridList() {
+    
     return (
         <div>
-        <p>the grid</p>
-            <Card/>
+        <Grid container spacing={1}>
+            {numbers.map( (i)=>
+                
+                <Grid item xs={12} md={4}>
+                    <Card/>
+                </Grid>
+            )}
+        </Grid>
         </div>
     )
 }
